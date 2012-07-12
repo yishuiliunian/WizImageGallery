77f80104d1329c634a666ef99a7ec2ed1b517463
dzpqzb <yishuiliunian@gamil.com>
2012-07-12 14:14:50 +0800

    将显示图片的视图更改为WizImageGalleryCell 其基类是UIScrollView以支持缩放

---
+++ WizImageGallery/WizImageGallery/WizImageGalleryCell.h
@@ -0,0 +1,13 @@
+//
+//  WizImageGalleryCell.h
+//  WizImageGallery
+//
+//  Created by wiz on 12-7-12.
+//  Copyright (c) 2012年 wiz.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizImageGalleryCell : UIScrollView
+
+@end
---
+++ WizImageGallery/WizImageGallery/WizImageGalleryCell.m
@@ -0,0 +1,31 @@
+//
+//  WizImageGalleryCell.m
+//  WizImageGallery
+//
+//  Created by wiz on 12-7-12.
+//  Copyright (c) 2012年 wiz.cn. All rights reserved.
+//
+
+#import "WizImageGalleryCell.h"
+
+@implementation WizImageGalleryCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
